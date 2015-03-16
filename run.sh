0cc66580c37eeef01af8b6ee9bbed66a780dc376
Willem Koopman <willem@portal.config>
2015-03-16 20:23:59 +0000

    The face detect script
    The HAAR cascades are taken from the opencv project, and are licensed
    accordingly.

---
+++ run.sh
@@ -0,0 +1,3 @@
+python facedetect.py --cascade=haar/haarcascade_eye.xml  raw/
+#python facedetect.py --cascade=haar/haarcascade_frontalface_alt.xml  /disk/raw/ 
+#python -m cProfile  facedetect.py --cascade=/opt/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml   0
